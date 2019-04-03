de89ef9fbe0b39e27c5ba4709daa7ec65de2c211
ryan <you@example.com>
2019-04-03 08:19:33 +0300

    updated

---
+++ src/app/user.ts
@@ -0,0 +1,11 @@
+export class User {
+  constructor(public name:string,
+     public login:string, 
+      public avatar_url:string,
+      public followers:number, 
+      public following:number,
+       public public_repository:number,
+        public html_url:string){}
+}
+
+
