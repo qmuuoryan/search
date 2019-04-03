a3e64a233be5d24ab35cfaae30a1ce5cd3b7d6b1
ryan <you@example.com>
2019-04-03 08:03:18 +0300

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { GitsearchComponent } from './gitsearch/gitsearch.component';
+import { RoutingModule } from './routing/routing.module';
+import { BoldenPipe } from './bolden.pipe';
+import { HighlightDirective } from './highlight.directive';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { GitsearchService } from './gitsearch.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    GitsearchComponent, 
+    IntroductionComponent,
+    BoldenPipe,
+    HighlightDirective,
+    IntroductionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    HttpModule,
+    FormsModule,
+    RoutingModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
