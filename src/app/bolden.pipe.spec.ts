3d50eb224ec0ddfe75b28bed7647d3b116c3bf6e
ryan <you@example.com>
2019-04-03 08:05:36 +0300

    updated

---
+++ src/app/bolden.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { BoldenPipe } from './bolden.pipe';
+
+describe('BoldedPipe', () => {
+  it('create an instance', () => {
+    const pipe = new BoldenPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
