bef85036650dd17120bb48cb1c2776e0e0534cb1
ryan <you@example.com>
2019-04-03 08:14:16 +0300

    updated

---
+++ src/app/gitsearch.service.spec.ts
@@ -0,0 +1,10 @@
+import {TestBed} from '@angular/core/testing';
+import {GitsearchService} from './gitsearch.service';
+
+describe('GitsearchService', () =>{
+    beforeEach(() => TestBed.configureTestingModule({}));
+    it ('should be created',() => {
+        const service: GitsearchService = TestBed.get(GitsearchService);
+        expect(service).toBeTruthy();
+    });
+})
